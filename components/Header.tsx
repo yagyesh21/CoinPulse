020188891cc15a7512397c58bd7ab48cd3ccddaf
Yagyesh2106 <yagyeshyadav2005@gmail.com>
2026-03-28 21:49:14 +0530

    added some changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,44 +1,45 @@
-'use client'
+'use client';
 
-import { cn } from "@/lib/utils"
-import Image from "next/image"
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const Header = () => {
-    const pathName = usePathname()
+  const pathname = usePathname();
+
   return (
     <header>
-      <div className="main-container inner flex items-center justify-between">
-        
-   
-        <Link href="/" className="flex items-center">
-          <Image
-            src="/logo.png"
-            alt="CoinPulse logo"
-            width={185}
-            height={40}
-            priority
-          />
+      <div className="main-container inner ">
+        <Link  href="/">
+          <Image className='' src="/logo.png" alt="CoinPulse logo" width={350} height={90} />
         </Link>
-          
-          <nav>
-            <Link href="/" className={cn('nav-link',{
-                'is-active' : pathName === '/',
-                'is-home' : true,
-            })}> Home </Link>
 
-            <p>Search Modal </p>
+        <nav>
+          <Link
+            href="/"
+            className={cn('nav-link', {
+              'is-active': pathname === '/',
+              'is-home': true,
+            })}
+          >
+            Home
+          </Link>
 
-            <Link href='/coins' className={cn('nav-link',{
-                'is-active' : pathName === '/coins',
-              
-            })}> All Coins </Link>
-          </nav>
-       </div>
-      
+          <p>Search Modal</p>
+
+          <Link
+            href="/coins"
+            className={cn('nav-link', {
+              'is-active': pathname === '/coins',
+            })}
+          >
+            All Coins
+          </Link>
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
